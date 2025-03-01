6791c571ab05963446212d2a6e4c2a4c611e4f58
enricliu <liuenyan6@bupt.edu.cn>
2025-03-01 21:49:34 +0800

    [feat] some debug tools.

--- cluster/src/utils/mod.rs
+++ cluster/src/utils/mod.rs
@@ -1,6 +1,17 @@
+mod position;
+pub use position::Position;
 use std::sync::atomic::AtomicUsize;
 
 pub static COUNTER: AtomicUsize = AtomicUsize::new(0);
+pub static DEBUG: bool = false;
+
 pub fn get_id() -> usize {
     COUNTER.fetch_add(1, std::sync::atomic::Ordering::SeqCst)
+}
+
+pub fn log(s: &str) {
+    if !DEBUG {
+        return;
+    }
+    println!("{s}");
 }
